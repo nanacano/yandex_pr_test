047378a569cd9265ac72e42e95d4b000da12e46d
nanacano <nanacano@rambler.ru>
2022-03-11 00:58:24 +0600

    Initial commit 2

--- src/test/java/general/DeliveryCalculator.java
+++ src/test/java/general/DeliveryCalculator.java
@@ -11,10 +11,6 @@ public class DeliveryCalculator {
      */
     public static double calculate(Distance distance, Dimensions dimensions
             , Workload workload, CargoFragility fragility){
-        System.out.println("distance:" + distance + " " +  distance.getPriceAddition());
-        System.out.println("dimensions:" +dimensions + " " + dimensions.getPriceAddition());
-        System.out.println("workload:"+ workload + " " + workload.getPriceMultiplier());
-        System.out.println("fragility:"+fragility + " " + fragility.getPriceAddition());
 
         if (distance == Distance.MORETHAN30 && fragility == CargoFragility.YES){
             return -1.0;
@@ -22,7 +18,6 @@ public class DeliveryCalculator {
 
         double sum = (distance.getPriceAddition() + dimensions.getPriceAddition() + fragility.getPriceAddition())
                 * workload.getPriceMultiplier();
-        System.out.println("(before check)sum " + sum);
         if (sum < 400.0){
             return 400.0;
         }
